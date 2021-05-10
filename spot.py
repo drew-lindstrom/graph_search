05a287f78bce09ef89761b061fdc6e487a90dc62
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-05-09 20:51:52 -0500

    Updated organization.

--- spot.py
+++ spot.py
@@ -1,3 +1,17 @@
+import pygame
+
+RED = (255, 0, 0)
+GREEN = (0, 0, 255)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+
 class Spot:
     def __init__(self, row, col, width, total_rows):
         self.row = row
