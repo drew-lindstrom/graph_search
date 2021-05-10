ffe120e2efa8f0b97cbe7d14257d0e86b7135117
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-05-10 14:22:14 -0500

    Initial commit.

---
+++ colors.py
@@ -0,0 +1,13 @@
+"""Color constants used to specify the status of each spot on the grid. 
+For example, a white spot means the spot is open while black means the spot is closed."""
+
+RED = (255, 0, 0)
+GREEN = (0, 0, 255)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
